3d5cd7e82a05956cbe99a20808db8b4543bf2fea
Victor Neves <victorfnss@hotmail.com>
2026-03-24 19:27:00 -0300

    add bucket s3 para versionamento tfstate

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "tf-state-bucket-556000333443-us-east-1-an"
+    key            = "projeto-ha/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
 provider "aws" {
